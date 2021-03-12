5b49933219427be8c56a65cfdb357af9d69e4583
zhoujian2 <zhoujian2@sensetime.com>
2021-03-12 17:54:18 +0800

    show how to create uml file

--- benchtest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# does post request benchtest using wrk
-wrk --threads=8 \
-    --connections=100 \
-    --duration=300s \
-    --script=http_post.lua \
-    http://localhost:8080/tesla/model3/inventory
